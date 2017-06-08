56b2460b9012b9183a4f924d178cdaf5506015dc
liyi <liyiorg@gmail.com>
2017-06-08 15:50:27 +0800

    update

--- src/main/java/com/github/liyiorg/mbg/suport/BaseServiceImpl.java
+++ src/main/java/com/github/liyiorg/mbg/suport/BaseServiceImpl.java
@@ -4,16 +4,26 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import com.github.liyiorg.mbg.bean.Page;
+import com.github.liyiorg.mbg.util.GenericsUtils;
 import com.github.liyiorg.mbg.util.MethodUtils;
 
-public abstract class BaseServiceImpl<Model, Example, PrimaryKey> implements BaseBLOBsService<Model, Example, PrimaryKey>{
+public abstract class BaseServiceImpl<Model, Example, PrimaryKey>
+		implements BaseBLOBsService<Model, Example, PrimaryKey> {
 
 	protected Object mapper;
-	
+
+	private Class<?> modelClass, exampleClass, primaryKeyClass;
+
+	{
+		modelClass = GenericsUtils.getSuperClassGenricType(this.getClass(), 0);
+		exampleClass = GenericsUtils.getSuperClassGenricType(this.getClass(), 1);
+		primaryKeyClass = GenericsUtils.getSuperClassGenricType(this.getClass(), 2);
+	}
+
 	@SuppressWarnings("unchecked")
-	private <T> T invokeExactMethod(String methodName,Object... args){
+	private <T> T invokeExactMethod(String methodName, Object[] args, Class<?>[] parameterTypes) {
 		try {
-			return (T) MethodUtils.invokeExactMethod(mapper, methodName, args);
+			return (T) MethodUtils.invokeExactMethod(mapper, methodName, args, parameterTypes);
 		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
@@ -23,98 +33,93 @@ public abstract class BaseServiceImpl<Model, Example, PrimaryKey> implements Bas
 		}
 		return null;
 	}
-	
+
+	private <T> T invokeExactMethod(String methodName, Object arg, Class<?> parameterType) {
+		return invokeExactMethod(methodName, new Object[] { arg }, new Class<?>[] { parameterType });
+	}
+
 	@Override
 	public long countByExample(Example example) {
-		if(example == null){
-			return invokeExactMethod("countByExample");
-		}
-		return invokeExactMethod("countByExample",example);
-		
+		return invokeExactMethod("countByExample", example, exampleClass);
+
 	}
 
 	@Override
 	public int deleteByExample(Example example) {
-		if(example == null){
-			return invokeExactMethod("deleteByExample");
-		}
-		return invokeExactMethod("deleteByExample",example);
+		return invokeExactMethod("deleteByExample", example, exampleClass);
 	}
 
 	@Override
 	public int deleteByPrimaryKey(PrimaryKey id) {
-		return invokeExactMethod("deleteByPrimaryKey",id);
+		return invokeExactMethod("deleteByPrimaryKey", id, primaryKeyClass);
 	}
 
 	@Override
 	public int insert(Model record) {
-		return invokeExactMethod("insert",record);
+		return invokeExactMethod("insert", record, modelClass);
 	}
 
 	@Override
 	public int insertSelective(Model record) {
-		return invokeExactMethod("insertSelective",record);
+		return invokeExactMethod("insertSelective", record, modelClass);
 	}
 
 	@Override
 	public List<Model> selectByExampleWithBLOBs(Example example) {
-		if(example == null){
-			return invokeExactMethod("selectByExampleWithBLOBs");
-		}
-		return invokeExactMethod("selectByExampleWithBLOBs",example);
+		return invokeExactMethod("selectByExampleWithBLOBs", example, exampleClass);
 	}
 
 	@Override
 	public List<Model> selectByExample(Example example) {
-		if(example == null){
-			return invokeExactMethod("selectByExample");
-		}
-		return invokeExactMethod("selectByExample",example);
+		return invokeExactMethod("selectByExample", example, exampleClass);
 	}
 
 	@Override
 	public Model selectByPrimaryKey(PrimaryKey id) {
-		return invokeExactMethod("selectByPrimaryKey", id);
+		return invokeExactMethod("selectByPrimaryKey", id, primaryKeyClass);
 	}
 
 	@Override
 	public int updateByExampleSelective(Model record, Example example) {
-		return invokeExactMethod("updateByExampleSelective", record,example);
+		return invokeExactMethod("updateByExampleSelective", new Object[] { record, example },
+				new Class<?>[] { modelClass, exampleClass });
 	}
 
 	@Override
 	public int updateByExampleWithBLOBs(Model record, Example example) {
-		return invokeExactMethod("updateByExampleWithBLOBs", record,example);
+		return invokeExactMethod("updateByExampleWithBLOBs", new Object[] { record, example },
+				new Class<?>[] { modelClass, exampleClass });
 	}
 
 	@Override
 	public int updateByExample(Model record, Example example) {
-		return invokeExactMethod("updateByExample", record,example);
+		return invokeExactMethod("updateByExample", new Object[] { record, example },
+				new Class<?>[] { modelClass, exampleClass });
 	}
 
 	@Override
 	public int updateByPrimaryKeySelective(Model record) {
-		return invokeExactMethod("updateByPrimaryKeySelective", record);
+		return invokeExactMethod("updateByPrimaryKeySelective", record, modelClass);
 	}
 
 	@Override
 	public int updateByPrimaryKeyWithBLOBs(Model record) {
-		return invokeExactMethod("updateByPrimaryKeyWithBLOBs", record);
+		return invokeExactMethod("updateByPrimaryKeyWithBLOBs", record, modelClass);
 	}
 
 	@Override
 	public int updateByPrimaryKey(Model record) {
-		return invokeExactMethod("updateByPrimaryKey", record);
+		return invokeExactMethod("updateByPrimaryKey", record, modelClass);
 	}
 
 	@Override
 	public Page<Model> selectByExample(Example example, Integer page, Integer size) {
-		if(example instanceof LimitInterface){
-			LimitInterface temp = (LimitInterface)example;
-			temp.setLimitStart((long)(page-1)*size);
-			temp.setLimitEnd((long)page*size);
+		if (example instanceof LimitInterface) {
+			LimitInterface temp = (LimitInterface) example;
+			temp.setLimitStart((long) (page - 1) * size);
+			temp.setLimitEnd((long) page * size);
 			List<Model> list = selectByExample(example);
-			
+
 			temp.setLimitStart(null);
 			temp.setOrderByClause(null);
 			long count = countByExample(example);
@@ -125,12 +130,12 @@ public abstract class BaseServiceImpl<Model, Example, PrimaryKey> implements Bas
 
 	@Override
 	public Page<Model> selectByExampleWithBLOBs(Example example, Integer page, Integer size) {
-		if(example instanceof LimitInterface){
-			LimitInterface temp = (LimitInterface)example;
-			temp.setLimitStart((long)(page-1)*size);
-			temp.setLimitEnd((long)page*size);
+		if (example instanceof LimitInterface) {
+			LimitInterface temp = (LimitInterface) example;
+			temp.setLimitStart((long) (page - 1) * size);
+			temp.setLimitEnd((long) page * size);
 			List<Model> list = selectByExampleWithBLOBs(example);
-			
+
 			temp.setLimitStart(null);
 			temp.setOrderByClause(null);
 			long count = countByExample(example);
@@ -139,5 +144,4 @@ public abstract class BaseServiceImpl<Model, Example, PrimaryKey> implements Bas
 		return null;
 	}
 
-	
 }
